c1cacdde791b71cf8b0099bee8322968080cfb43
ale-php <alexandre@indev.net.br>
2017-02-02 18:11:36 -0200

    websockt vue2 com php

--- src/MyApp/Chat.php
+++ src/MyApp/Chat.php
@@ -18,6 +18,7 @@ class Chat implements MessageComponentInterface {
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
+
         $numRecv = count($this->clients) - 1;
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
             , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
@@ -26,6 +27,8 @@ class Chat implements MessageComponentInterface {
             if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
                 $client->send($msg);
+            }else{
+                $client->send($msg); // envia para o rementent da menssagem
             }
         }
     }
